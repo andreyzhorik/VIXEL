7821a58c4f1053248020bb81bbbdb94c187f0b6e
andrey <abdalovandrey95@gmail.com>
2025-12-17 10:41:07 -0800

    Update protection.js

--- protection.js
+++ protection.js
@@ -1,20 +1,4 @@
-let isDirty = false;
-
-const textInput = document.getElementById('textInput');
-const saveBtn = document.getElementById('saveBtn');
-
-textInput.addEventListener('input', () => {
-  isDirty = true;
-});
-
-saveBtn.addEventListener('click', () => {
-  isDirty = false;
-  alert('Changes saved!');
-});
-
 window.addEventListener('beforeunload', (e) => {
-  if (isDirty) {
-    e.preventDefault();
-    e.returnValue = '';
-  }
+  e.preventDefault();
+  e.returnValue = ''; // This triggers the browser's native confirm dialog
 });
